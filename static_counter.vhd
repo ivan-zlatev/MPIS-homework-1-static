42f8c39026dc837b33235a66c9ede626f90ce70d
ivan-zlatev <ivanzlatev90@gmail.com>
2014-03-16 15:43:26 +0200

    final draft

--- static_counter.vhd
+++ static_counter.vhd
@@ -3,14 +3,14 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
  
 entity counter is
-    generic (N: integer := 3);													--number of digits
+    generic (N: integer := 3);	--number of digits
     port (
-	clock : in std_logic;															--clk signal
-	reset : in std_logic;															--reset signal
-	q0 : out unsigned (3 downto 0);												--output digit0
-	q1 : out unsigned (3 downto 0);												--output digit1
-	q2 : out unsigned (3 downto 0);												--output digit2
-	carry : out std_logic															--carry signal
+	clock : in std_logic;	--clk signal
+	reset : in std_logic;	--reset signal
+	q0 : out unsigned (3 downto 0);	--output digit0
+	q1 : out unsigned (3 downto 0);	--output digit1
+	q2 : out unsigned (3 downto 0);	--output digit2
+	carry : out std_logic	--carry signal
 );
 end counter;
  
@@ -29,15 +29,15 @@ begin
 			carry <= '0';
 		else
 			if rising_edge(clock) then
-				if (temp0 = 9) then
+				if (temp0 = 9) then	--check for digit 0 overflow
 					temp0 <= INITIAL_STATE;
 					temp1 <= temp1 +1;
-					if (temp1 = 9) then
+					if (temp1 = 9) then	--check for digit 1 overflow
 						temp1 <= INITIAL_STATE;
 						temp2 <= temp2 +1;
-						if (temp2 = 9) then
+						if (temp2 = 9) then	--check for digit 2 overflow
 							temp2 <= INITIAL_STATE;
-							carry <= '1';
+							carry <= '1';	--generate carry signal after digit 2 overflow
 						else
 							temp2 <= temp2 +1;
 						end if;
--- static_counter_test.vhd
+++ static_counter_test.vhd
@@ -47,38 +47,46 @@ begin
   
     -- stimulus process
     stim_proc: process
-    begin      
+    begin
+		-- Err1 - testing for values when reset = 1
       reset <= '1';
       wait until falling_edge(clock);
       assert q0 = to_unsigned(0, WIDTH) report "Err1";
 		assert q1 = to_unsigned(0, WIDTH) report "Err1";
 		assert q2 = to_unsigned(0, WIDTH) report "Err1";
 
-		-- testing for counting up
+		-- Err2 - testing for initial values after reset <= 0
 		reset <= '0';
 		assert q0 = to_unsigned(0, WIDTH) report "Err2 DIG0_0";
 		assert q1 = to_unsigned(0, WIDTH) report "Err2 DIG1_0";
 		assert q2 = to_unsigned(0, WIDTH) report "Err2 DIG2_0";
+		-- Err3 - testing for counting up (0 to 999)
 		for a in 0 to 9 loop
-			assert q2 = to_unsigned(a, WIDTH) report "Err3 DIG2 shoud be " & integer'image(a) & ", but is " & integer'image(to_integer(q2));
+			assert q2 = to_unsigned(a, WIDTH) report "Err3 DIG2 should be " & integer'image(a) & ", but is " & integer'image(to_integer(q2));
 			for b in 0 to 9 loop
-				assert q1 = to_unsigned(b, WIDTH) report "Err3 DIG1 shoud be " & integer'image(b) & ", but is " & integer'image(to_integer(q1));
+				assert q1 = to_unsigned(b, WIDTH) report "Err3 DIG1 should be " & integer'image(b) & ", but is " & integer'image(to_integer(q1));
 				for c in 0 to 9 loop
-					assert q0 = to_unsigned(c, WIDTH) report "Err3 DIG0 shoud be " & integer'image(c) & ", but is " & integer'image(to_integer(q0));
+					assert q0 = to_unsigned(c, WIDTH) report "Err3 DIG0 should be " & integer'image(c) & ", but is " & integer'image(to_integer(q0));
 					wait until falling_edge(clock);
 				end loop;
 			end loop;
 		end loop;
+		
+		-- Err4 - testing for carry signal generation
+		assert carry = '1' report "Err4 CARRY signal is not generated after DIG2 overflow";
+		-- Err5 - testing for carry signal duration
+		wait until falling_edge(clock);
+		assert carry = '0' report "Err4 CARRY duration is longer than expected";
 
-		-- testing for reset signal
+		-- Err6 - testing for output values after reset signal
 		wait for 1us;
 		wait until falling_edge(clock);
 		reset <='1';
 		wait until falling_edge(clock);
 		reset <='0';
-		assert q0 = to_unsigned(0, WIDTH) report "Err4 DIG0_0";
-		assert q1 = to_unsigned(0, WIDTH) report "Err4 DIG1_0";
-		assert q2 = to_unsigned(0, WIDTH) report "Err4 DIG2_0";
+		assert q0 = to_unsigned(0, WIDTH) report "Err6 DIG0_0";
+		assert q1 = to_unsigned(0, WIDTH) report "Err6 DIG1_0";
+		assert q2 = to_unsigned(0, WIDTH) report "Err6 DIG2_0";
 		
       clock_run <= false;
       wait;
